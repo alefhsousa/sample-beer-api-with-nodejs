07e1dc72aabe58b658ba0392df9b08589a9758c5
Alefh Sousa <alefh.sousa@gmail.com>
2017-08-18 16:09:21 -0300

    app nodejs

---
+++ app.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var mongo = require('mongoose');
+var bodyParser = require('body-parser');
+var app = express();
+
+var port = 9000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Passo 0 - conexão com mongo
+//docker 
+//var mongoaddr = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':27017/beersapi';
+var mongoaddr = 'mongodb://localhost:27017/beersapi';
+console.log(mongoaddr);
+//mongo.connect(mongoaddr);
+
+mongo.connect(mongoaddr, { useMongoClient: true })
+
+ 
+//Passo 1 - Esquema da collection do Mongo
+var beerSchema = mongo.Schema({
+	name : { type: String, required: true }, 
+    brand : { type: String, required: true },
+    category: {type: String, enum: ['IPA', 'Lager', 'Ale']}, 
+    update_at: { type: Date, default: Date.now },
+    create_at: { type: Date, default: Date.now },
+});
+
+//Passo 2 - Beer da aplicação
+var Beer = mongo.model('Beer', beerSchema);
+
+app.get('/v1/beers', function(req, res){
+	Beer.find(function(err, beers) {
+		if (err) {
+			res.json(err);
+		} else {
+            if(beers.length != 0){
+                res.json(beers);
+            }else {
+                res.status('204').send();
+                res.end();
+            } 
+			
+		}
+	})
+});
+
+app.get("/v1/beers/:brand?", function (req, res) {
+    var brand = req.params.brand;
+    var brand2 = req.query.brand;
+    console.log("rodando..: " + brand2)
+	Beer.find({'brand': brand}, function(err, regs) {
+		if (err) {
+			console.log(err);
+			res.send(err);
+		} else {
+			res.json(regs);
+		}
+	});
+});
+
+//POST - Adiciona um registro
+app.post("/v1/beers", function (req, res) {
+	var register = new Beer({
+		'name' : req.body.name,
+        'brand' : req.body.brand,
+        'category': req.body.category,
+	});
+	register.save(function (err) {
+		if (err) {
+			console.log(err);
+            res.send(err);
+			res.end();
+		}
+    });
+    res.send(register)
+    // res.status('201').location('/beers/' + register._id)
+	res.end();
+});
+
+app.put("/v1/beers/:id", function (req, res) {
+	Beer.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err)  {
+    	return next(err);
+    } else {
+    	res.json(post);
+    }
+  });
+});
+
+
+app.delete("/v1/beers/:id", function (req, res) {
+    Beer.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+       if (err) return next(err);
+       res.json(post);
+     });
+});
+
+//Porta de escuta do servidor
+app.listen(port, function() {
+	console.log('App listen on: ' + port);
+});
